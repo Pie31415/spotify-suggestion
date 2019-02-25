c68575d5b8c27ab19f6c81c01e9e0d87aa2f67bf
Andy <andy999coc@gmail.com>
2019-02-25 17:54:48 -0500

    recommended playlist fix

--- spotify_rec.py
+++ spotify_rec.py
@@ -6,13 +6,13 @@ from sklearn.tree import DecisionTreeClassifier
 import spotipy
 import spotipy.util as util
 
-spotify_discover_weekly_id = 'XXXX'
+spotify_playlist_id = '37i9dQZEVXcHLzzqRkcgrd'
 
-username = 'XXXX'
+username = '21jytlvp5orepkujthaapp7cy'
 scope = 'user-library-read user-library-modify playlist-read-private playlist-modify-private playlist-modify-public'
-client_id = 'XXXX'
-client_secret = 'XXX'
-redirect_uri = 'XXXX'
+client_id = '13113033cada447986869fcda8b4a6f0'
+client_secret = 'c7b8dcecc92b4a06abbb93ababa83e08'
+redirect_uri = 'http://localhost:8080/'
 
 token = util.prompt_for_user_token(
     username,
@@ -43,12 +43,12 @@ csv_columns = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness
                'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'type',
                'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature', 'like']
 
-features = []
-discover_weekly_features = []
+features = []  # list to contain the audio features of playlists
+spotify_playlist_features = []  # list to contain the AF of selected spotify playlist
 
 if token:
     sp = spotipy.Spotify(auth=token)
-
+    sp.trace = False
     playlists = sp.user_playlists(username)
 
     # for each playlist in user's library
@@ -61,40 +61,40 @@ if token:
 
             while tracks['next']:
                 songs = tracks['items']
-                tracks = sp.next(tracks)
 
-                # print(playlist['name'], "--------------------------", len(songs))
                 # add fav song ids and names to list
                 for i in range(0, len(songs)):
                     if songs[i]['track']['id'] is not None:
                         audio_features = sp.audio_features(songs[i]['track']['id'])
                         for feature in audio_features:
                             if feature is not None:
-                                if playlist['name'] == 'Favourite':
-                                    feature['like'] = 1
-                                else:
+                                if playlist['name'] == 'NotFavourite':
                                     feature['like'] = 0
+                                else:
+                                    feature['like'] = 1
                                 features.append(feature)
+                tracks = sp.next(tracks)
 
     currentPath = os.getcwd()
     csv_file = currentPath + "playlists.csv"
     writePlaylistToCSV(csv_file, csv_columns, features)
 
-    discover_weekly = sp.user_playlist(username, spotify_discover_weekly_id, fields='tracks,next')
-    tracks = discover_weekly['tracks']
+    spotify_playlist = sp.user_playlist(username, spotify_playlist_id, fields='tracks,next')
+    tracks = spotify_playlist['tracks']
     songs = tracks['items']
     for i in range(0, len(songs)):
         if songs[i]['track']['id'] is not None:
             audio_features = sp.audio_features(songs[i]['track']['id'])
             for feature in audio_features:
                 if feature is not None:
-                    discover_weekly_features.append(feature)
+                    spotify_playlist_features.append(feature)
 
     # actual machine learning
-    recommended_ids = []
 
-    # df for weekly playlists
-    df = pd.DataFrame(data=discover_weekly_features, columns=csv_columns)
+    recommended_ids = []  # list to contain ids of recommended songs
+
+    # df for playlists
+    df = pd.DataFrame(data=spotify_playlist_features, columns=csv_columns)
     testData = df.drop(['type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature', 'like'], axis=1)
     # print(testData.head())
 
@@ -115,13 +115,21 @@ if token:
         if recommend == 1:
             recommended_ids.append(df.loc[i]['id'])
 
-    # create RECOMMENDED Playlist
-    sp.trace = False
-    recommend_playlist = sp.user_playlist_create(username,
-                                                 'Recommended')
-    playlist_id = recommend_playlist['id']
+    # is there already a recommended playlist?
+    recommended_playlist_id = ''
+    my_playlists = sp.current_user_playlists(limit=50)
+    for playlist in my_playlists['items']:
+        if playlist['name'] == 'Recommended':
+            recommended_playlist_id = playlist['id']
+
+    if recommended_playlist_id == '':
+        # create RECOMMENDED Playlist
+        recommend_playlist = sp.user_playlist_create(username,
+                                                     'Recommended')
+        recommended_playlist_id = recommend_playlist['id']
+
     results = sp.user_playlist_add_tracks(username,
-                                          playlist_id,
+                                          recommended_playlist_id,
                                           recommended_ids)
 else:
     print("Can't get token for", username)
